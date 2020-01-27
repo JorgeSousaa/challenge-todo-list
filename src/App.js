c80098b0b4eb7b5f6af9c8c473e9188ab52358ea
JorgeSousaa <jorge_sousa@live.com.pt>
2020-01-27 21:46:56 +0000

    Created base layout

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <div className="Title">
+          TodoList
+        </div>
+      </div>
+      <div className="AddField">
+        <div className="AddTask">
+          <input type="text" placeholder="I need a task..." />
+          <input type="button" value="Create" />
+        </div>
+      </div>
+      <div className="TaskWindow">
+        <div className="TaskView">
+          <div className="TaskTitle">
+            Tasks
+          </div>
+          <div className="TaskList">
+            <div className="TaskComponent">
+              <div className="TaskLine">
+                <div className="TaskComponentCheck">
+                  <input type="checkbox" value="" />
+                </div>
+                <div className="TaskComponentDescription">
+                  __________________________________________________
+                  </div>
+                <div className="TaskComponentButton">
+                  <input type="button" value="Edit" />
+                  <input type="button" value="Delete" />
+                </div>
+              </div>
+            </div>
+            <div className="HideTask">
+              Hide complete
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
